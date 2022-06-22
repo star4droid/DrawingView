d7e95ea6902f7d90abc401bc817796ac7f2cc2ce
star4droid <81539602+star4droid@users.noreply.github.com>
2022-06-22 19:53:37 +0200

    Add files via upload

--- library/DrawingView.java
+++ library/DrawingView.java
@@ -6,16 +6,95 @@ import android.graphics.Paint;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.view.MotionEvent;
+import android.graphics.PointF;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.PorterDuff;
 import java.util.LinkedList;
-class DrawingView extends View {
+import android.graphics.drawable.GradientDrawable;
+import android.widget.LinearLayout;
+class DrawingView extends LinearLayout {
 public DrawingView(Context ctx)  {
 super(ctx);
- setLayerType(View.LAYER_TYPE_HARDWARE,null);
+setLayerType(View.LAYER_TYPE_HARDWARE,null);
+ rulerp1 = new LinearLayout(ctx);
+ rulerp2 = new LinearLayout(ctx);
+this.addView(rulerp1);
+this.addView(rulerp2);
+ rulerp1.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)90, (int)1, 0xFF607D8B, Color.TRANSPARENT));
+ rulerp2.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)90, (int)1, 0xFF607D8B, Color.TRANSPARENT));
+ rulerp1.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+ rulerp2.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+ rulerp1.setX(100);
+ rulerp1.setY(100);
+ rulerp2.setY(100);
+ rulerp2.setX(100);
+ dxx= rulerp1.getX()-rulerp2.getX();
+ dyy= rulerp1.getY()-rulerp2.getY();
+//onTouch drag1
+final DrawingView cc=this;
+rulerp1.setOnTouchListener(new OnTouchListener() {
+				PointF DownPT = new PointF();
+				PointF StartPT = new PointF();
+		//float dx= rulerp1.getX()-rulerp2.getX();
+		//float dy= rulerp1.getY()-rulerp2.getY();
+				@Override public boolean onTouch(View _view, MotionEvent event) {
+						switch (event.getAction()) {
+								case MotionEvent.ACTION_MOVE:
+			cc.invalidate();
+			PointF mv = new PointF(event.getX() - DownPT.x, event.getY() - DownPT.y);
+								_view.setX((int)(StartPT.x+mv.x));
+								_view.setY((int)(StartPT.y+mv.y));
+		if(showH&&(TYPE==TYPES.CIRCLE)){
+			rulerp2.setX((int)(StartPT.x+mv.x-dxx));
+			rulerp2.setY((int)(StartPT.y+mv.y+dyy));
+			}
+								StartPT = new PointF(_view.getX(), _view.getY());
+								break; //xenon
+								case MotionEvent.ACTION_DOWN : 
+								DownPT.x = event.getX();
+								DownPT.y = event.getY();
+								StartPT = new PointF(_view.getX(), _view.getY());
+								break;
+								case MotionEvent.ACTION_UP : break;
+								default : break;
+						} return true;
+				}});
+				//onTouch drag2
+rulerp2.setOnTouchListener(new OnTouchListener() {
+				PointF DownPT = new PointF();
+				PointF StartPT = new PointF();
+				@Override public boolean onTouch(View _view, MotionEvent event) {
+						switch (event.getAction()) {
+								case MotionEvent.ACTION_MOVE:
+			cc.invalidate();
+			PointF mv = new PointF(event.getX() - DownPT.x, event.getY() - DownPT.y);
+								_view.setX((int)(StartPT.x+mv.x));
+								_view.setY((int)(StartPT.y+mv.y));
+								StartPT = new PointF(_view.getX(), _view.getY());
+								break; //xenon
+								case MotionEvent.ACTION_DOWN : DownPT.x = event.getX();
+								DownPT.y = event.getY();
+								StartPT = new PointF(_view.getX(), _view.getY());
+								break;
+								case MotionEvent.ACTION_UP :
+			dxx=rulerp1.getX()-rulerp2.getX();
+			dyy=rulerp1.getY()-rulerp2.getY();
+								  break;
+								default : break;
+						} return true;
+				}});
+		rulerp1.setVisibility(View.INVISIBLE);
+		rulerp2.setVisibility(View.INVISIBLE);
+		setBackgroundColor(Color.TRANSPARENT);
+				//end
 }
 boolean start=false;
+float dxx;
+float dyy;
 	private float strokeWidth=5;
+	private LinearLayout rulerp1;
+	private LinearLayout rulerp2;
+	private boolean showH=false;
 	private int pcolor=Color.BLACK;
 	private float xx=0;
 	public enum STYLE {
@@ -63,6 +142,53 @@ for(PathS p:pz){
 if(p.path != null) cv.drawPath(p.path,p.mPaint);
 }
 }
+//ruler
+if(showH&&((TYPE==TYPES.CIRCLE)|(TYPE==TYPES.LINE))){
+rulerp1.setVisibility(View.VISIBLE);
+rulerp2.setVisibility(View.VISIBLE);
+rulerp1.setLayoutParams(new LinearLayout.LayoutParams(25,25));
+if (TYPE==TYPES.LINE){
+rulerp1.setLayoutParams(new LinearLayout.LayoutParams(50,50));
+Path pth=new Path();
+pth.moveTo(rulerp1.getX()+rulerp1.getMeasuredWidth()/2,rulerp1.getY()+rulerp1.getMeasuredHeight()/2);
+pth.lineTo(rulerp2.getX()+rulerp2.getMeasuredWidth()/2,rulerp2.getY()+rulerp2.getMeasuredHeight()/2);
+			Paint pi = new Paint();
+			pi.setStrokeWidth(4);
+			pi.setColor(Color.BLACK);
+			pi.setStyle(Paint.Style.STROKE);
+			pi.setStrokeCap(Paint.Cap.ROUND);
+pi.setStrokeJoin(Paint.Join.ROUND);
+pi.setDither(true);
+pi.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+cv.drawPath(pth,pi);
+} else if((TYPE==TYPES.CIRCLE)&&showH){
+Path pth = new Path();
+Paint pi = new Paint();
+			pi.setStrokeWidth(2);
+			pi.setColor(Color.BLACK);
+			pi.setStyle(Paint.Style.STROKE);
+			pi.setStrokeCap(Paint.Cap.ROUND);
+pi.setStrokeJoin(Paint.Join.ROUND);
+pi.setDither(true);
+pi.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+float xc= rulerp2.getX()+rulerp2.getMeasuredWidth()/2;
+float yc=rulerp2.getY()+rulerp2.getMeasuredHeight()/2;
+float tx1=(float)((rulerp1.getX()+rulerp1.getMeasuredWidth()/2)-xc);
+			float tx2=(float)((rulerp1.getY()+rulerp1.getMeasuredHeight()/2)-yc);
+float r =(float)Math.sqrt((tx1*tx1)+(tx2*tx2));
+pth.addCircle(rulerp1.getX()+rulerp1.getMeasuredWidth()/2,rulerp1.getY()+rulerp1.getMeasuredHeight()/2,r,Path.Direction.CCW);
+cv.drawPath(pth,pi);
+}
+} else {
+rulerp1.setVisibility(View.INVISIBLE);
+ rulerp2.setVisibility(View.INVISIBLE);
+}
+//end
+}
+public void ShowHelper(boolean b){
+showH=b;
+invalidate();
+//not ready
 }
 public void setPenSize(float w){
 	strokeWidth=w;
@@ -115,6 +241,7 @@ public boolean CanRedo(){
 }
 public void setDrawType(TYPES x){
 	TYPE=x;
+	invalidate();
 }
 
 	@Override
@@ -128,11 +255,13 @@ public void setDrawType(TYPES x){
 			pi.setStrokeWidth(2);
 			pi.setColor(Color.BLACK);
 			pi.setStyle(Paint.Style.STROKE);
+			if(!showH){
 			pth.moveTo(event.getX()-40,event.getY());
 			pth.lineTo(event.getX()+40,event.getY());
 			pth.moveTo(event.getX(),event.getY()-40);
 			pth.lineTo(event.getX(),event.getY()+40);
 			pth.addCircle(event.getX(),event.getY(),30,Path.Direction.CCW);
+			}
 			PathS pa=new PathS(pth,pi,2,Color.BLACK);
 			pz.add(pa);
 			}
@@ -191,10 +320,18 @@ path.moveTo(event.getX(),event.getY());
 			} else {
 			if(TYPE==TYPES.CIRCLE){
 			path.reset();
+			if(showH&&false){
+			//its not ready
+			float tx1=(float)((rulerp1.getX()+rulerp1.getMeasuredWidth()/2)-cx);
+			float tx2=(float)((rulerp1.getY()+rulerp1.getMeasuredHeight()/2)-cy);
+			float r =(float)Math.sqrt((tx1*tx1)+(tx2*tx2));
+			path.addCircle(rulerp1.getX()+rulerp1.getMeasuredWidth()/2,rulerp1.getY()+rulerp1.getMeasuredHeight()/2,r,Path.Direction.CCW);
+			} else {
 			double xmx = Math.abs((double)(cx-xx));
 	double ymy = Math.abs((double)(cy-yy));
 	double r = Math.sqrt((xmx*xmx)+(ymy*ymy));
 			path.addCircle(xx,yy,(float)r,Path.Direction.CCW);
+			}
 			} else {
 			if(TYPE==TYPES.TRIANGLE1){
 			path.reset();
@@ -221,7 +358,11 @@ path.moveTo(event.getX(),event.getY());
 			if(TYPE==TYPES.LINE){
 			path.reset();
 			path.moveTo(xx,yy);
+			if(showH){
+			path.lineTo(cx-(xx-rulerp1.getX()),cy);
+			} else {
 			path.lineTo(cx,cy);
+			}
 			}
 			}//end last
 			}
